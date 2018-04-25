e3bdad8f6045fac8298e1e583667bc1330b6d38b
andrew lupton <aflupton@gmail.com>
2018-04-25 09:28:02 -0700

    add changes

---
+++ ScrabbleScore.Tests/ModelTests/ScrabbleScoreTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScrabbleScore.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ScrabbleScore.TestTools
+{
+  [TestClass]
+  public class ScrabbleScoreTest
+  {
+    [TestMethod]
+    public void 
+  }
+}
